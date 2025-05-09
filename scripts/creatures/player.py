b538a1e51ac7f47138df0460f2ba29db78a3daa1
lordinator37 <benjamin.short11@education.nsw.gov.au>
2025-05-09 12:43:20 +1000

    velocity

--- scripts/creatures/player.py
+++ scripts/creatures/player.py
@@ -27,6 +27,9 @@ class Player(enity.Entity):
 
         sprint = input.keyInput(py.K_LCTRL)
 
+        byebyeFriction = input.keyInput(py.K_l)
+        byebyeFriction.onDown = self.goodbyeFriction
+
         sprint.onDown = self.sprintToggle
 
         up.onDown = self.move_up
@@ -39,7 +42,7 @@ class Player(enity.Entity):
         left.onUp = self.left_stop
         right.onUp = self.right_stop
 
-        #self.debug = True
+        self.debug = True
 
         self.keys = {
             "up": False,
@@ -50,6 +53,7 @@ class Player(enity.Entity):
 
         self.sprinting = False
         self.humanoid["speed"] = 1
+        self.friction = 0.5
 
     def move_left(self, key):
         self.keys["left"] = True
@@ -75,6 +79,9 @@ class Player(enity.Entity):
             self.humanoid["speed"] = 2
         else:
             self.humanoid["speed"] = 1
+    def goodbyeFriction(self, key):
+        #self.friction = 0
+        self.zIndex += 1
     
     def collide(self, previousPosition):
         self.obj.x = previousPosition.x; self.obj.y = previousPosition.y
@@ -82,24 +89,52 @@ class Player(enity.Entity):
     def update(self, dt):
         previousPosition = math.Vector2(self.obj.x, self.obj.y)
         if self.keys["left"] == True:
-            self.obj.x -= self.humanoid["speed"]
+            self.velocity.x = -self.humanoid["speed"]
         if self.keys["right"] == True:
-            self.obj.x += self.humanoid["speed"]
+            self.velocity.x = self.humanoid["speed"]
         if self.keys["up"] == True:
-            self.obj.y -= self.humanoid["speed"]
+            self.velocity.y = -self.humanoid["speed"]
         if self.keys["down"] == True:
-            self.obj.y += self.humanoid["speed"]
+            self.velocity.y = self.humanoid["speed"]
 
+        self.obj.x += self.velocity.x; self.obj.y += self.velocity.y
+        if self.velocity.x > 0:
+            self.velocity.x -= self.friction
+        elif self.velocity.x < 0:
+            self.velocity.x += self.friction
+        
+        if self.velocity.y > 0:
+            self.velocity.y -= self.friction
+        elif self.velocity.y < 0:
+            self.velocity.y += self.friction
+
+        if self.velocity.x >= 0 and self.velocity.x <= 0.2:
+            self.velocity.x = 0
+        
+        if self.velocity.y >= 0 and self.velocity.y <= 0.2:
+            self.velocity.y = 0
+        
         localselfPosition = math.Vector2(self.obj.x, self.obj.y).toScreenSpace(self.scene.camera, py.display.get_surface())
         localSelfRect = py.Rect(localselfPosition.x, localselfPosition.y, self.obj.w, self.obj.h)
 
+        closet = 99999
+        chosen = None
+
         for tile in self.scene.mapTiles:
-            tile.debugColour = py.Color(255, 0, 0)
+            tile.debugColour = py.Color(0, 255, 0)
 
             localtilePosition = math.Vector2(tile.obj.x, tile.obj.y).toScreenSpace(self.scene.camera, py.display.get_surface())
             localTileRect = py.Rect(localtilePosition.x, localtilePosition.y, tile.obj.w, tile.obj.h)
 
             if localSelfRect.colliderect(localTileRect):
-                tile.debugColour = py.Color(0, 255, 0)
+                tile.debugColour = py.Color(255, 0, 0)
                 if (self.zIndex + 1) == tile.zIndex or self.zIndex == tile.zIndex:
                     self.collide(previousPosition)
+
+                distance = (localtilePosition - localselfPosition).magnitude()
+                if distance < closet:
+                    closet = distance
+                    chosen = tile
+        if chosen:
+            chosen.debugColour = py.Color(0, 0, 255)
+            print(chosen.zIndex)
